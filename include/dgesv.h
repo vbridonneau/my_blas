22d9d1600065f27c1f3168371a1591d1a9542c18
Abel Calluaud <abelcd@protonmail.com>
2019-11-16 17:16:22 +0100

    add dgesv

---
+++ include/dgesv.h
@@ -0,0 +1,7 @@
+#ifndef DEF_DGESV_H
+#define DEF_DGESV_H
+
+#include "algonum.h"
+
+
+#endif//DEF_DGETRF_H
---
+++ src/dgesv.c
@@ -0,0 +1,10 @@
+#include "dgesv.h"
+#include "algonum.h"
+#include <assert.h>
+#include <stdbool.h>
+
+int dgesv_(int n, int nrhs, double *a, int lda, int *ipiv, double *b, int *ldb, int *info) {
+	  // lu fact
+	  // trsm 1
+	  // trsm 2
+}
