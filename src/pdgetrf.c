b8c72276ab4a5ee271e9ced7d1f948d76f58bd10
Bridonneau Vincent <vincent.bridonneau@enseirb-matmeca.fr>
2019-12-08 15:34:26 +0100

    Gathering implemented

--- src/pdgetrf.c
+++ src/pdgetrf.c
@@ -32,6 +32,7 @@ void my_pdgetrf(const CBLAS_LAYOUT Order, int M, int N, double* A, int lda ) {
   /* FIXME : Create a data type for band to send */
   /* by A and one to send by Asub.               */
   /* Or think about array of array.              */
+  /* Warning : Not relevant in first             */
   /***********************************************/
 
   /* Matrices */
@@ -52,7 +53,7 @@ void my_pdgetrf(const CBLAS_LAYOUT Order, int M, int N, double* A, int lda ) {
   MPI_Type_commit( &band_type );
   MPI_Type_commit( &last_band_type );
 
-  /* Split */
+  /**************************** Split *********************/
 
   /* Begin by scattering complete band to all processes */
   for (int i = 0; i < NT - 1; ++i) {
@@ -64,21 +65,20 @@ void my_pdgetrf(const CBLAS_LAYOUT Order, int M, int N, double* A, int lda ) {
     if (rank == 0) {
       int shiftA = (NT - 1) * size * block_size * lda;
 
-      /* 0 is the last, so it receives the last band in 'Asub' */
-      if (rlast == 0) {
-        double *recvAsub  = Asub + (NT - 1) * band_size;
-        double *ptrAshift = A + shiftA;
-        int     last_size = (is_last_complete) ? band_size : last_band_size;
-        memcpy(recvAsub, ptrAshift, size);
-      } 
-      /* Ohterwise 0 sends to all untill the last one a complete band
+      /* 0 receives its the last band in 'Asub' */
+      double *recvAsub  = Asub + (NT - 1) * band_size;
+      double *ptrAshift = A + shiftA;
+      int     last_size = (!is_last || is_last_complete) ? band_size : last_band_size;
+      memcpy(recvAsub, ptrAshift, size);
+    
+      /* 0 sends to all untill the last complete band
        * and it manages the last band case */
-      else {
-        /* rlast - 1 complete band */
-        for(int rnk = 1; rnk < rlast; ++rnk) {
-          MPI_Send(A + shiftA + rnk * block_size * lda, 1, band_type, rnk, 0, MPI_COMM_WORLD);
-        }
-        /* Last block info to send it last band properly */
+      /* rlast - 1 complete band */
+      for(int rnk = 1; rnk < rlast; ++rnk) {
+        MPI_Send(A + shiftA + rnk * block_size * lda, 1, band_type, rnk, 0, MPI_COMM_WORLD);
+      }
+      if(rlast != 0) {
+        /* Last band info to send it properly */
         MPI_Datatype ltype = (is_last_complete) ? band_type : last_band_type;
         double *sendAptr   = A + shiftA + rlast * block_size * lda;
         int last_size      = (is_last_complete) ? band_size : last_band_size;
@@ -94,13 +94,47 @@ void my_pdgetrf(const CBLAS_LAYOUT Order, int M, int N, double* A, int lda ) {
     }
   }
 
-  /* Computation */
+  /********************** Computation ************************/
 
-  /* Gather data */
+  /********************** Gather data ************************/
   for (int i = 0; i < NT - 1; ++i) {
     MPI_Gather(Asub + i * band_size, 1, band_type, A + i * size * band_size, size, band_type, 0, MPI_COMM_WORLD);
   }
 
+  /* Then gather by hand from processes that have one more band */
+  if (NT == NT_treated) {
+    /* 0 receives data from all other processes */
+    if (rank == 0) {
+      int shiftA = (NT - 1) * size * band_size;
+
+      /* First 0 takes its data from Asub */
+      double *ptrAshift = A + shiftA;
+      double *sendAsub  = Asub + (NT - 1) * band_size;
+      int     last_size = (!is_last || is_last_complete) ? band_size : last_band_size;
+      memcpy(ptrAshift, sendAsub, last_size);
+
+      /* Receive from all others until rlast excluded */
+      for (int rnk = 1; rnk < rlast; ++rnk) {
+        MPI_Recv(A + shiftA + rnk * band_size, 1, band_type, rnk, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+      }
+      /* Treat last band case */
+      if (rlast != 0) {
+        MPI_Datatype ltype = (is_last_complete) ? band_type : last_band_type;
+        double *recvAptr   = A + shiftA + rlast * band_size;
+        int     last_size  = (is_last_complete) ? band_size : last_band_size;
+        MPI_Recv(recvAptr, 1, ltype, rlast, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+      }      
+    }
+    /* Others send their last band to 0 */
+    else if (rank <= rlast) {
+      MPI_Datatype rtype     = (is_last && !is_last_complete) ? last_band_type : band_type;
+      double      *sendAsub  = (NT - 1) * band_size;
+      int          last_size = (is_last && !is_last_complete) ? last_band_size : band_size;
+      MPI_Send(sendAsub, 1, rtype, 0, 0, MPI_COMM_WORLD);
+    }
+  }
+
+  /*********************** Free Section ***********************/
   free(Asub);
 
   /* Free types*/
